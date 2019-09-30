49e095f367ad878dc00b2727e250667153df9691
Alex <45263160+alexwender1@users.noreply.github.com>
2019-09-30 12:41:01 -0300

    Create weather-pt_BR.ts
    I contribute to Brazilian Portuguese (pt-BR) translation. Ask the Deepin Team to add your software to the store.

---
+++ source/i18n/weather-pt_BR.ts
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR" sourcelanguage="en">
+<context>
+    <name>ForecastApplet</name>
+    <message>
+        <source>N/A</source>
+        <translation>N/A</translation>
+    </message>
+    <message>
+        <source>Error: %1</source>
+        <translation>Erro: %1</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherClient</name>
+    <message>
+        <source>NA</source>
+        <translation>NA</translation>
+    </message>
+    <message>
+        <source>Humidity %1%
+</source>
+        <translation>Umidade %1%
+</translation>
+    </message>
+    <message>
+        <source>Clouds %1%
+</source>
+        <translation>Nuvens %1%
+</translation>
+    </message>
+    <message>
+        <source>N</source>
+        <translation>N</translation>
+    </message>
+    <message>
+        <source>NE</source>
+        <translation>NE</translation>
+    </message>
+    <message>
+        <source>E</source>
+        <translation>L</translation>
+    </message>
+    <message>
+        <source>SE</source>
+        <translation>SE</translation>
+    </message>
+    <message>
+        <source>S</source>
+        <translation>S</translation>
+    </message>
+    <message>
+        <source>SW</source>
+        <translation>SO</translation>
+    </message>
+    <message>
+        <source>W</source>
+        <translation>O</translation>
+    </message>
+    <message>
+        <source>NW</source>
+        <translation>NO</translation>
+    </message>
+    <message>
+        <source>Wind %1 %2, %3
+</source>
+        <translation>Vento %1 %2, %3
+</translation>
+    </message>
+    <message>
+        <source>m/s</source>
+        <translation>m/s</translation>
+    </message>
+    <message>
+        <source>Sunrise at %1
+</source>
+        <translation>Nascer do Sol %1
+</translation>
+    </message>
+    <message>
+        <source>Sunset at %1
+</source>
+        <translation>Pôr do Sol %1
+</translation>
+    </message>
+    <message>
+        <source>Updated at %1</source>
+        <translation>Atualizado em %1</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherPlugin</name>
+    <message>
+        <source> Updating... </source>
+        <translation> Atualizando... </translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Configurações</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Atualizar</translation>
+    </message>
+    <message>
+        <source>Weather</source>
+        <translation>Clima</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherSettingDialog</name>
+    <message>
+        <source>Weather</source>
+        <translation>Clima</translation>
+    </message>
+    <message>
+        <source>City Information</source>
+        <translation>Informação do Município</translation>
+    </message>
+    <message>
+        <source>City</source>
+        <translation>Município</translation>
+    </message>
+    <message>
+        <source>Country</source>
+        <translation>País</translation>
+    </message>
+    <message>
+        <source>You can also input cityid from &lt;a href=&quot;https://openweathermap.org/city&quot; style=&quot;color: yellow&quot;&gt;openweathermap.org&lt;/a&gt; (the number in the URL of your city page) to avoid unambiguous result. </source>
+        <translation>Você pode inserir o código do Município de &lt;a href=&quot;https://openweathermap.org/city&quot; style=&quot;color: yellow&quot;&gt;openweathermap.org&lt;/a&gt; (o número na URL da página da sua cidade) para evitar resultados inesperados. </translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Opções</translation>
+    </message>
+    <message>
+        <source>Interval</source>
+        <translation>Intervalo</translation>
+    </message>
+    <message>
+        <source>999 min</source>
+        <translation>999 min</translation>
+    </message>
+    <message>
+        <source>Due to the limitations of openweathermap.org for free account, you have to use your own appid to set shorter time interval.</source>
+        <translation>Devido às limitações do OpenWeatherMap.org para a conta gratuita, você deve usar o seu próprio AppID para um intervalo menor.</translation>
+    </message>
+    <message>
+        <source>Units</source>
+        <translation>Unidades</translation>
+    </message>
+    <message>
+        <source>metric</source>
+        <translation>métrico</translation>
+    </message>
+    <message>
+        <source>imperial</source>
+        <translation>imperial</translation>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Idioma</translation>
+    </message>
+    <message>
+        <source>System Language</source>
+        <translation>Idioma do Sistema</translation>
+    </message>
+    <message>
+        <source>You may need to restart the dock to make language setting effective.</source>
+        <translation>Pode ser necessário reiniciar a dock para o idioma entrar em vigor.</translation>
+    </message>
+    <message>
+        <source>appid from openweathermap.org</source>
+        <translation>AppID de OpenWeatherMap.org</translation>
+    </message>
+    <message>
+        <source>AppID</source>
+        <translation>AppID</translation>
+    </message>
+    <message>
+        <source>The plugin default appid is proviede by the author. Click to learn how to get your own appid.</source>
+        <translation>O AppID padrão do plug-in é fornecido pelo autor. Clique para saber como obter o seu próprio AppID.</translation>
+    </message>
+</context>
+</TS>
