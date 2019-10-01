011056d12574ef6d22cbd515fc48a08bbe5354ed
alvarosamudio <alvarosamudio@criptext.com>
2019-10-01 07:31:41 -0500

    Translations to Spanish
    Spanish translations were added

---
+++ source/i18n/weather-es.ts
@@ -0,0 +1,196 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es" sourcelanguage="en">
+<context>
+    <name>ForecastApplet</name>
+    <message>
+        <source> MM/dd 
+ddd</source>
+        <translation>ddd
+ dd.MM </translation>
+    </message>
+    <message>
+        <source>N/A</source>
+        <translation>N/A</translation>
+    </message>
+    <message>
+        <source>Error: %1</source>
+        <translation>Error: %1</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherClient</name>
+    <message>
+        <source>NA</source>
+        <translation>NA</translation>
+    </message>
+    <message>
+        <source>Humidity %1%
+</source>
+        <translation>Humedad %1%
+</translation>
+    </message>
+    <message>
+        <source>Clouds %1%
+</source>
+        <translation>Nubes %1%
+</translation>
+    </message>
+    <message>
+        <source>N</source>
+        <translation>N</translation>
+    </message>
+    <message>
+        <source>NE</source>
+        <translation>NE</translation>
+    </message>
+    <message>
+        <source>E</source>
+        <translation>E</translation>
+    </message>
+    <message>
+        <source>SE</source>
+        <translation>SE</translation>
+    </message>
+    <message>
+        <source>S</source>
+        <translation>S</translation>
+    </message>
+    <message>
+        <source>SW</source>
+        <translation>SO</translation>
+    </message>
+    <message>
+        <source>W</source>
+        <translation>O</translation>
+    </message>
+    <message>
+        <source>NW</source>
+        <translation>NO</translation>
+    </message>
+    <message>
+        <source>Wind %1 %2, %3
+</source>
+        <translation>Viento %1 %2, %3
+</translation>
+    </message>
+    <message>
+        <source>m/s</source>
+        <translation>м/s</translation>
+    </message>
+    <message>
+        <source>Sunrise at %1
+</source>
+        <translation>Salida del sol %1
+</translation>
+    </message>
+    <message>
+        <source>Sunset at %1
+</source>
+        <translation>Puesta de sol %1
+</translation>
+    </message>
+    <message>
+        <source>Updated at %1</source>
+        <translation>Actualizado en %1</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherPlugin</name>
+    <message>
+        <source> Updating... </source>
+        <translation> Actualizando... </translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Refresh</translation>
+    </message>
+    <message>
+        <source>Weather</source>
+        <translation>Tiempo</translation>
+    </message>
+</context>
+<context>
+    <name>WeatherSettingDialog</name>
+    <message>
+        <source>Weather</source>
+        <translation>Tiempo</translation>
+    </message>
+    <message>
+        <source>City Information</source>
+        <translation>Información de Ciudad</translation>
+    </message>
+    <message>
+        <source>City</source>
+        <translation>Ciudad</translation>
+    </message>
+    <message>
+        <source>Country</source>
+        <translation>País</translation>
+    </message>
+    <message>
+        <source>You can also input cityid from &lt;a href=&quot;https://openweathermap.org/city&quot; style=&quot;color: yellow&quot;&gt;openweathermap.org&lt;/a&gt; (the number in the URL of your city page) to avoid unambiguous result. </source>
+        <translation>También puede introducir cityid desde &lt;a href=&quot;https://openweathermap.org/city&quot; style=&quot;color: yellow&quot;&gt;openweathermap.org&lt;/a&gt; (el número en la URL de su página de ciudad) para evitar resultados erroneos. </translation>
+    </message>
+    <message>
+        <source>Options</source>
+        <translation>Opciones</translation>
+    </message>
+    <message>
+        <source>Interval</source>
+        <translation>Intervalo</translation>
+    </message>
+    <message>
+        <source>999 min</source>
+        <translation>999 min</translation>
+    </message>
+    <message>
+        <source>Due to the limitations of openweathermap.org for free account, you have to use your own appid to set shorter time interval.</source>
+        <translation>Debido a las limitaciones de openweathermap.org para una cuenta gratuita, usted tiene que usar su propio appid para establecer un intervalo de tiempo más corto. </translation>
+    </message>
+    <message>
+        <source>Units</source>
+        <translation>Unidades</translation>
+    </message>
+    <message>
+        <source>metric</source>
+        <translation>métrica</translation>
+    </message>
+    <message>
+        <source>imperial</source>
+        <translation>imperial</translation>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Idioma</translation>
+    </message>
+    <message>
+        <source>System Language</source>
+        <translation>Idioma del sistema</translation>
+    </message>
+    <message>
+        <source>You may need to restart the dock to make language setting effective.</source>
+        <translation>Es posible que deba reiniciar el acoplamiento para que la configuración del idioma sea efectiva.</translation>
+    </message>
+    <message>
+        <source>appid from openweathermap.org</source>
+        <translation>appid de openweathermap.org</translation>
+    </message>
+    <message>
+        <source>AppID</source>
+        <translation>AppID</translation>
+    </message>
+    <message>
+        <source>The plugin default appid is proviede by the author. Click to learn how to get your own appid.</source>
+        <translation>La aplicación por defecto del plugin es proporcionada por el autor. Haga clic para aprender a obtener su propio appid.</translation>
+    </message>
+</context>
+</TS>
